7aeb46808f85a1bd7e487be47daaf45ca813336e
Asep Deffy <asepdeffy@telkomuniversity.ac.id>
2024-09-24 15:47:31 +0700

    Ganti jadi gaes

--- ProgramDira.java
+++ ProgramDira.java
@@ -2,6 +2,6 @@ public class ProgramDira {
 
     public static void main(String[] args) {
         System.out.print("Hello ");
-        System.out.print("Word");
+        System.out.print("Gaes");
     }
 }
